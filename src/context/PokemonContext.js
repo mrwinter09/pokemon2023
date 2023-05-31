4ddf0914909daff29a1eacf48988e30353ac750c
mrwinter09 <ivan.winter09@gmail.com>
2023-05-31 13:45:44 +0200

    almost

--- src/context/PokemonContext.js
+++ src/context/PokemonContext.js
@@ -4,7 +4,38 @@ export const PokemonContext = createContext({})
 
 function PokemonContextComponent({children}) {
 
-  
+  class PokemonMoves {
+    constructor(name, move){
+      this.name = name;
+      this.move = move;
+    }
+    attackA() {
+     const {name, move} = this;
+     return `${name} is doing the ${move}`
+    }
+  }
+
+  class PokemonA extends PokemonMoves {
+    attackA() {
+      return 'Take That!!!!'
+    }
+  }
+
+  class PokemonB extends PokemonMoves {
+    attackB() {
+      return 'Take That!!!!'
+    }
+  }
+
+  const data = {
+   fight: PokemonMoves
+  }
+
+  return (
+    <PokemonContext.Provider value={data}>
+      {children}
+      </PokemonContext.Provider>
+  )
 }
 
 export default PokemonContextComponent
