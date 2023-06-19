c890ed29c9f0fb9cda891256ccab94cb996476b5
mrwinter09 <ivan.winter09@gmail.com>
2023-06-19 19:53:52 +0200

    prettier

--- src/pages/BattlePage/BattlePage.js
+++ src/pages/BattlePage/BattlePage.js
@@ -1,18 +1,55 @@
-import React, { useContext } from 'react';
-import {PokemonContext} from '../../context/PokemonContext'
+import React, { useContext } from 'react'
+import { PokemonContext } from '../../context/PokemonContext'
 import './BattlePage.css'
 
-
 function BattlePage() {
-  const {firstPokemonResult, secondPokemonResult, pokemonSpeciesName, abilityDescription, setPokemonHpScoreB, pokemonHpScoreB, setPokemonHpScoreA, pokemonHpScoreA, startButton, setPokemonBattleId, setStartButton, battleStatsA, setBattleStatsA, battleStatsB, setBattleStatsB, reset, gameover, scoreCount, winnerA, winnerB, numberA, numberB, pokemonId, generateRandomNumber, randomNum, ProgressDivs, icons, colors, totalProgressBars, progressStatusArrayLength, progressStatusArray } = useContext(PokemonContext)
- 
-  const player = {
-      1: firstPokemonResult.pokemonName,
-      2: secondPokemonResult.pokemonName
-    }
+  const {
+    firstPokemonResult,
+    secondPokemonResult,
+    pokemonSpeciesName,
+    abilityDescription,
+    setPokemonHpScoreB,
+    pokemonHpScoreB,
+    setPokemonHpScoreA,
+    pokemonHpScoreA,
+    startButton,
+    setPokemonBattleId,
+    setStartButton,
+    battleStatsA,
+    setBattleStatsA,
+    battleStatsB,
+    setBattleStatsB,
+    reset,
+    gameover,
+    scoreCount,
+    winnerA,
+    winnerB,
+    numberA,
+    numberB,
+    pokemonId,
+    generateRandomNumber,
+    randomNum,
+    ProgressDivs,
+    icons,
+    colors,
+    totalProgressBars,
+    progressStatusArrayLength,
+    progressStatusArray,
+  } = useContext(PokemonContext)
 
-  let testCountA = (firstPokemonResult.pokemonStats / 6 * secondPokemonResult.pokemonStats / firstPokemonResult.pokemonStats).toFixed(0)
-  let testCountB = (secondPokemonResult.pokemonStats / 6 * firstPokemonResult.pokemonStats / secondPokemonResult.pokemonStats).toFixed(0)
+  const player = {
+    1: firstPokemonResult.pokemonName,
+    2: secondPokemonResult.pokemonName,
+  }
+
+  let testCountA = (
+    ((firstPokemonResult.pokemonStats / 6) * secondPokemonResult.pokemonStats) /
+    firstPokemonResult.pokemonStats
+  ).toFixed(0)
+  let testCountB = (
+    ((secondPokemonResult.pokemonStats / 6) * firstPokemonResult.pokemonStats) /
+    secondPokemonResult.pokemonStats
+  ).toFixed(0)
 
   const firstSymbols = icons[firstPokemonResult.pokemonType]
   const firstColor = colors[firstPokemonResult.pokemonType]
@@ -21,147 +58,207 @@ function BattlePage() {
 
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <h1 className='battle-title'>Battle Page</h1>
-      </header>
-      <div className="split-bar">
-       <div>
-        <p className='pokemonScoreFirst'>{firstPokemonResult.pokemonHp} {pokemonHpScoreA}<img src={firstPokemonResult.pokemonBattleIcon} alt=''></img><span className={winnerA ? '' : 'hidden'}>{pokemonSpeciesName}</span></p>
-       <div className='power-bar'>
-          {progressStatusArray.map((item, index) => {
-            return (
-              <ProgressDivs
-                key={item}
-                backgroundColorStyle={index < numberA ? "red" : "black"}
-                flexValue={
-                  totalProgressBars / progressStatusArrayLength - 0.005
-                }
-              />
-            );
-          })}
-        </div>
-       </div>
-       <div>
-       <p className='pokemonScoreSecond'><span className={winnerB ? '' : 'hidden'}>{pokemonSpeciesName}</span><img src={secondPokemonResult.pokemonBattleIcon} alt=''></img>{secondPokemonResult.pokemonHp} {pokemonHpScoreB}</p>
-        <div
-          className='power-bar'
-        >
-          {progressStatusArray.map((item, index) => {
-            return (
-              <ProgressDivs
-                key={item}
-                backgroundColorStyle={index < numberB ? "red" : "black"}
-                flexValue={
-                  totalProgressBars / progressStatusArrayLength - 0.005
-                }
-              />
-            );
-          })}
-        </div>
-       </div>
-
-      </div>
-        <div className='pokemonCardContainerBattlePage'>
-
-        <div className="split">
-              <div className="left">
-          <div style={{border: '5px solid'+ firstColor}} className="card card--pokemon">
-           <div className="card-image">
-             <div className="card-image-container">
-               <img src={firstPokemonResult.pokemonResultImage} alt={firstPokemonResult.pokemonName}/>
-             </div>
-           </div>
-           <div className="card-content">
-             <div className="main">
-               <div className="title ">{firstPokemonResult.pokemonName}</div>
-               <hr style={{backgroundColor: firstColor}} />
-               <div className="pokemon-hp">{firstPokemonResult.pokemonHp} {firstPokemonResult.pokemonStats}</div>
-             </div>
-             <div className="stats">
-               <div className="column nudge">
-               <img className='symbol' src={firstSymbols} alt={firstPokemonResult.pokemonType}></img>
-                 <span style={{backgroundColor: firstColor}} className="tag is-warning">Type</span>
-               </div>
-
-               <div className="column center-column">{firstPokemonResult.pokemonWeight} lbs
-                 <span style={{backgroundColor: firstColor}} className="tag is-warning">Weight</span>
-               </div>
-               <div className="column">{firstPokemonResult.pokemonHeight} m
-                 <span style={{backgroundColor: firstColor}} className="tag is-warning">Height</span>
-               </div>
-             </div>
-           </div>
-        </div>
-          </div>
-          <p className="abilityDescriptionText">{abilityDescription}</p>
+      <div className="App">
+        <header className="App-header">
+          <h1 className="battle-title">Battle Page</h1>
+        </header>
+        <div className="split-bar">
+          <div>
+            <p className="pokemonScoreFirst">
+              {firstPokemonResult.pokemonHp} {pokemonHpScoreA}
+              <img src={firstPokemonResult.pokemonBattleIcon} alt=""></img>
+              <span className={winnerA ? '' : 'hidden'}>{pokemonSpeciesName}</span>
+            </p>
+            <div className="power-bar">
+              {progressStatusArray.map((item, index) => {
+                return (
+                  <ProgressDivs
+                    key={item}
+                    backgroundColorStyle={index < numberA ? 'red' : 'black'}
+                    flexValue={totalProgressBars / progressStatusArrayLength - 0.005}
+                  />
+                )
+              })}
             </div>
-
-          <div className="center">
-          <header className="App-header">
-          <h1 className='battle-title vs'>VS</h1>
-          </header>
           </div>
+          <div>
+            <p className="pokemonScoreSecond">
+              <span className={winnerB ? '' : 'hidden'}>{pokemonSpeciesName}</span>
+              <img src={secondPokemonResult.pokemonBattleIcon} alt=""></img>
+              {secondPokemonResult.pokemonHp} {pokemonHpScoreB}
+            </p>
+            <div className="power-bar">
+              {progressStatusArray.map((item, index) => {
+                return (
+                  <ProgressDivs
+                    key={item}
+                    backgroundColorStyle={index < numberB ? 'red' : 'black'}
+                    flexValue={totalProgressBars / progressStatusArrayLength - 0.005}
+                  />
+                )
+              })}
+            </div>
+          </div>
+        </div>
+        <div className="pokemonCardContainerBattlePage">
+          <div className="split">
+            <div className="left">
+              <div style={{ border: '5px solid' + firstColor }} className="card card--pokemon">
+                <div className="card-image">
+                  <div className="card-image-container">
+                    <img src={firstPokemonResult.pokemonResultImage} alt={firstPokemonResult.pokemonName} />
+                  </div>
+                </div>
+                <div className="card-content">
+                  <div className="main">
+                    <div className="title ">{firstPokemonResult.pokemonName}</div>
+                    <hr style={{ backgroundColor: firstColor }} />
+                    <div className="pokemon-hp">
+                      {firstPokemonResult.pokemonHp} {firstPokemonResult.pokemonStats}
+                    </div>
+                  </div>
+                  <div className="stats">
+                    <div className="column nudge">
+                      <img className="symbol" src={firstSymbols} alt={firstPokemonResult.pokemonType}></img>
+                      <span style={{ backgroundColor: firstColor }} className="tag is-warning">
+                        Type
+                      </span>
+                    </div>
 
-            <div className="split">
+                    <div className="column center-column">
+                      {firstPokemonResult.pokemonWeight} lbs
+                      <span style={{ backgroundColor: firstColor }} className="tag is-warning">
+                        Weight
+                      </span>
+                    </div>
+                    <div className="column">
+                      {firstPokemonResult.pokemonHeight} m
+                      <span style={{ backgroundColor: firstColor }} className="tag is-warning">
+                        Height
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
             <p className="abilityDescriptionText">{abilityDescription}</p>
-              <div className="right">
-          <div style={{border: '5px solid'+ secondColor}} className="card card--pokemon">
-           <div className="card-image">
-             <div className="card-image-container">
-               <img src={secondPokemonResult.pokemonResultImage} alt={secondPokemonResult.pokemonName}/>
-             </div>
-           </div>
-           <div className="card-content">
-             <div className="main">
-               <div className="title ">{secondPokemonResult.pokemonName}</div>
-               <hr style={{backgroundColor: secondColor}} />
-               <div className="pokemon-hp">{secondPokemonResult.pokemonHp} {secondPokemonResult.pokemonStats}</div>
-             </div>
-             <div className="stats">
-               <div className="column nudge">
-               <img className='symbol' src={secondSymbols} alt={secondPokemonResult.pokemonType}></img>
-                 <span style={{backgroundColor: secondColor}} className="tag is-warning">Type</span>
-               </div>
-
-               <div className="column center-column">{secondPokemonResult.pokemonWeight} lbs
-                 <span style={{backgroundColor: secondColor}} className="tag is-warning">Weight</span>
-               </div>
-               <div className="column">{secondPokemonResult.pokemonHeightB} m
-                 <span style={{backgroundColor: secondColor}} className="tag is-warning">Height</span>
-               </div>
-             </div>
-           </div>
-        </div>
           </div>
+
+          <div className="center">
+            <header className="App-header">
+              <h1 className="battle-title vs">VS</h1>
+            </header>
+          </div>
+
+          <div className="split">
+            <p className="abilityDescriptionText">{abilityDescription}</p>
+            <div className="right">
+              <div style={{ border: '5px solid' + secondColor }} className="card card--pokemon">
+                <div className="card-image">
+                  <div className="card-image-container">
+                    <img src={secondPokemonResult.pokemonResultImage} alt={secondPokemonResult.pokemonName} />
+                  </div>
+                </div>
+                <div className="card-content">
+                  <div className="main">
+                    <div className="title ">{secondPokemonResult.pokemonName}</div>
+                    <hr style={{ backgroundColor: secondColor }} />
+                    <div className="pokemon-hp">
+                      {secondPokemonResult.pokemonHp} {secondPokemonResult.pokemonStats}
+                    </div>
+                  </div>
+                  <div className="stats">
+                    <div className="column nudge">
+                      <img className="symbol" src={secondSymbols} alt={secondPokemonResult.pokemonType}></img>
+                      <span style={{ backgroundColor: secondColor }} className="tag is-warning">
+                        Type
+                      </span>
+                    </div>
+
+                    <div className="column center-column">
+                      {secondPokemonResult.pokemonWeight} lbs
+                      <span style={{ backgroundColor: secondColor }} className="tag is-warning">
+                        Weight
+                      </span>
+                    </div>
+                    <div className="column">
+                      {secondPokemonResult.pokemonHeightB} m
+                      <span style={{ backgroundColor: secondColor }} className="tag is-warning">
+                        Height
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
           </div>
-
-      <section>
-
-        <div className={!startButton ? 'button-start' : 'button-start hidden'}>
-           <h2 className='title-margin'>Press Start</h2>
-           <button onClick={() => {setStartButton(!startButton)}} type='button' className='start-btn'>START</button>
         </div>
 
-        <div className={startButton ? 'button-ab' : 'button-ab hidden'}>
-          <h2 className={gameover ? 'title-margin' : 'title-margin hidden'}>Game Over</h2>
-          <button onClick={() => {reset(); scoreCount(); setPokemonBattleId(pokemonId(1000)); setPokemonHpScoreA(firstPokemonResult.pokemonStats); setPokemonHpScoreB(secondPokemonResult.pokemonStatsB)}} className={gameover ? 'start-btn' : 'hidden'}>Reset</button>
+        <section>
+          <div className={!startButton ? 'button-start' : 'button-start hidden'}>
+            <h2 className="title-margin">Press Start</h2>
+            <button
+              onClick={() => {
+                setStartButton(!startButton)
+              }}
+              type="button"
+              className="start-btn"
+            >
+              START
+            </button>
+          </div>
 
-          <div className={gameover ? 'hidden' : ''}>
-          <div className="center">
-          <header className="App-header">
-          <h1 className='battleplayer'>{player[randomNum]}</h1>
-          </header>
+          <div className={startButton ? 'button-ab' : 'button-ab hidden'}>
+            <h2 className={gameover ? 'title-margin' : 'title-margin hidden'}>Game Over</h2>
+            <button
+              onClick={() => {
+                reset()
+                scoreCount()
+                setPokemonBattleId(pokemonId(1000))
+                setPokemonHpScoreA(firstPokemonResult.pokemonStats)
+                setPokemonHpScoreB(secondPokemonResult.pokemonStatsB)
+              }}
+              className={gameover ? 'start-btn' : 'hidden'}
+            >
+              Reset
+            </button>
+
+            <div className={gameover ? 'hidden' : ''}>
+              <div className="center">
+                <header className="App-header">
+                  <h1 className="battleplayer">{player[randomNum]}</h1>
+                </header>
+              </div>
+              <button
+                disabled={randomNum === 2}
+                onClick={() => {
+                  setBattleStatsB(battleStatsB - 1)
+                  setPokemonHpScoreB(pokemonHpScoreB - testCountA)
+                  generateRandomNumber(1, 2)
+                }}
+                type="button"
+                className={randomNum === 1 ? 'video-game-button' : 'video-game-disable'}
+              >
+                A
+              </button>
+              <button
+                disabled={randomNum === 1}
+                onClick={() => {
+                  setBattleStatsA(battleStatsA - 1)
+                  setPokemonHpScoreA(pokemonHpScoreA - testCountB)
+                  generateRandomNumber(1, 2)
+                }}
+                type="button"
+                className={randomNum === 2 ? 'video-game-button' : 'video-game-disable'}
+              >
+                B
+              </button>
+            </div>
           </div>
-          <button disabled={randomNum === 2} onClick={() => {setBattleStatsB(battleStatsB - 1); setPokemonHpScoreB(pokemonHpScoreB - testCountA); generateRandomNumber(1, 2)}} type='button' className={randomNum === 1 ? 'video-game-button' : 'video-game-disable'}>A</button>
-           <button disabled={randomNum === 1} onClick={() => {setBattleStatsA(battleStatsA - 1); setPokemonHpScoreA(pokemonHpScoreA - testCountB); generateRandomNumber(1, 2)}} type='button' className={randomNum === 2 ? 'video-game-button' : 'video-game-disable'}>B</button>
-          </div>
-        </div>
-      </section>
+        </section>
       </div>
     </>
-  );
+  )
 }
 
-export default BattlePage;
+export default BattlePage
--- src/pages/Profile/Profile.js
+++ src/pages/Profile/Profile.js
@@ -1,33 +1,37 @@
-import React, { useContext } from 'react';
-import { AuthContext } from '../../context/AuthContext';
+import React, { useContext } from 'react'
+import { AuthContext } from '../../context/AuthContext'
 import './Profile.css'
-import { PokemonContext } from '../../context/PokemonContext';
-
+import { PokemonContext } from '../../context/PokemonContext'
 
 function Profile() {
-  const {valueEmail, valueName,} = useContext(AuthContext);
-  const {firstPokemonResult} = useContext(PokemonContext);
+  const { valueEmail, valueName } = useContext(AuthContext)
+  const { firstPokemonResult } = useContext(PokemonContext)
 
   return (
     <>
-    <header className="App-header">
-        <h1 className='profile-title'>Profile Page</h1>
+      <header className="App-header">
+        <h1 className="profile-title">Profile Page</h1>
       </header>
- 
       <section>
         <h2>{valueName}</h2>
-        <p><strong>Gebruikersnaam:</strong> {valueName}</p>
-        <p><strong>Email: </strong>{valueEmail}</p>
+        <p>
+          <strong>Gebruikersnaam:</strong> {valueName}
+        </p>
+        <p>
+          <strong>Email: </strong>
+          {valueEmail}
+        </p>
       </section>
       <section>
         <h2>Number of battles</h2>
-        <div className='profile-footer'>
-        <p><img src={firstPokemonResult.pokemonBattleIcon} alt=''></img> have won 10 battles</p>
+        <div className="profile-footer">
+          <p>
+            <img src={firstPokemonResult.pokemonBattleIcon} alt=""></img> have won 10 battles
+          </p>
         </div>
-       
       </section>
     </>
-  );
+  )
 }
 
-export default Profile;
+export default Profile
