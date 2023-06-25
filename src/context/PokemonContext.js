c4461ae17e43de96cd6dc27492e47caf53fff865
mrwinter09 <ivan.winter09@gmail.com>
2023-06-25 14:11:51 +0200

    color normal

--- src/context/PokemonContext.js
+++ src/context/PokemonContext.js
@@ -1,5 +1,5 @@
-import React, { createContext, useState, useEffect } from "react";
-import axios from 'axios';
+import React, { createContext, useState, useEffect } from 'react'
+import axios from 'axios'
 import bug from '../assets/icons/bug.png'
 import dark from '../assets/icons/dark.png'
 import dragon from '../assets/icons/dragon.png'
@@ -21,9 +21,8 @@ import water from '../assets/icons/water.png'
 
 export const PokemonContext = createContext({})
 
-function PokemonContextComponent({children}) {
-
-// Pokemon API and Data call
+function PokemonContextComponent({ children }) {
+  // Pokemon API and Data call
 
   const [pokemon, setPokemon] = useState('')
   const [pokemonBattleId, setPokemonBattleId] = useState(0)
@@ -34,122 +33,121 @@ function PokemonContextComponent({children}) {
   const [pokemonSpecies, setPokemonSpecies] = useState([])
   const [pokemonSpeciesName, setPokemonSpeciesName] = useState('')
 
-// Pokemon A
+  // Pokemon A
   const [firstPokemonResult, setFirstPokemonResult] = useState({})
 
-// Pokemon B
-   const [secondPokemonResult, setSecondPokemonResult] = useState({})
+  // Pokemon B
+  const [secondPokemonResult, setSecondPokemonResult] = useState({})
 
   useEffect(() => {
     function callPokemon(value) {
-      const results = pokeNames.filter((user)=> {
+      const results = pokeNames.filter((user) => {
         return value && user && user.name && user.name.toLowerCase().includes(value.toLowerCase())
       })
-      setResults(results);
+      setResults(results)
     }
     callPokemon(pokemon)
-  },
-  [pokemon])
+  }, [pokemon])
 
-  useEffect(()=> {
+  useEffect(() => {
     const fetchData = () => {
       fetch('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
-        .then(response => response.json())
+        .then((response) => response.json())
         .then((json) => {
           setPokeNames(json.results)
         })
     }
-      fetchData()
-  },[]);
+    fetchData()
+  }, [])
 
   useEffect(() => {
     async function fetchDataPokemon() {
       try {
-        const responseSpecies = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`);
+        const responseSpecies = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemon}`)
         setPokemonSpecies(responseSpecies.data.flavor_text_entries['6'].flavor_text)
         setPokemonSpeciesName(responseSpecies.data.names['0'].name)
       } catch (e) {
-        console.error(e);
+        console.error(e)
       }
     }
-    if(pokemon){
+    if (pokemon) {
       fetchDataPokemon()
     }
-  },[pokemon]);
-
+  }, [pokemon])
 
   useEffect(() => {
-        function creatPlayer(value) {
-           const playerObject =  {
-             pokemonResult: value.data,
-             pokemonName: value.data.name,
-             pokemonResultImage: value.data.sprites.other['official-artwork'].front_default,
-             pokemonBattleIcon: value.data.sprites.versions['generation-vii'].icons.front_default,
-             pokemonType: value.data.types['0'].type.name,
-             pokemonHp: value.data.stats['0'].stat.name,
-             pokemonWeight: value.data.weight,
-             pokemonHeight: value.data.height,
-             pokemonStats: value.data.stats['0'].base_stat,
-             pokeAbilityName: value.data.abilities['0'].ability.name,
-           }
-           return playerObject
+    function creatPlayer(value) {
+      const playerObject = {
+        pokemonResult: value.data,
+        pokemonName: value.data.name,
+        pokemonResultImage: value.data.sprites.other['official-artwork'].front_default,
+        pokemonBattleIcon: value.data.sprites.versions['generation-vii'].icons.front_default,
+        pokemonType: value.data.types['0'].type.name,
+        pokemonHp: value.data.stats['0'].stat.name,
+        pokemonWeight: value.data.weight,
+        pokemonHeight: value.data.height,
+        pokemonStats: value.data.stats['0'].base_stat,
+        pokeAbilityName: value.data.abilities['0'].ability.name,
+      }
+      return playerObject
+    }
+    async function fetchDataPokemon(a, b) {
+      try {
+        if (a) {
+          const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${a}`)
+          const playerOne = creatPlayer(response)
+          setFirstPokemonResult(playerOne)
+          const responseAbility = await axios.get(
+            `https://pokeapi.co/api/v2/ability/${firstPokemonResult.pokeAbilityName}`
+          )
+          const abilities = responseAbility.data.effect_entries['1'].effect
+          setAbilityDescription(abilities)
         }
-        async function fetchDataPokemon(a, b) {
-           try {
-             if(a) {
-              const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${a}`);
-              const playerOne = creatPlayer(response)
-              setFirstPokemonResult(playerOne)
-              const responseAbility = await axios.get(`https://pokeapi.co/api/v2/ability/${firstPokemonResult.pokeAbilityName}`);
-              const abilities = responseAbility.data.effect_entries['1'].effect;
-              setAbilityDescription(abilities)
-             }
-             if(b) {
-               const responsePokemonBattle = await axios.get(`https://pokeapi.co/api/v2/pokemon/${b}`);
-               const playerTwo = creatPlayer(responsePokemonBattle)
-               setSecondPokemonResult(playerTwo)
-               setPokemonHpScoreB(playerTwo.pokemonStats)
-             }
-           } catch (e) {
-             console.error(e);
-           }
+        if (b) {
+          const responsePokemonBattle = await axios.get(`https://pokeapi.co/api/v2/pokemon/${b}`)
+          const playerTwo = creatPlayer(responsePokemonBattle)
+          setSecondPokemonResult(playerTwo)
+          setPokemonHpScoreB(playerTwo.pokemonStats)
         }
-        if(pokemon){
-           fetchDataPokemon(pokemon, pokemonBattleId )
-        }
-    },[pokemon, pokemonBattleId]);
+      } catch (e) {
+        console.error(e)
+      }
+    }
+    if (pokemon) {
+      fetchDataPokemon(pokemon, pokemonBattleId)
+    }
+  }, [pokemon, pokemonBattleId])
 
-//Pokemon Battle State
+  //Pokemon Battle State
 
- const [active, setActive] = useState(false)
+  const [active, setActive] = useState(false)
 
   const [startButton, setStartButton] = useState(false)
   const [battleStatsA, setBattleStatsA] = useState(6)
   const [battleStatsB, setBattleStatsB] = useState(6)
   const [totalScore, setTotalScore] = useState(0)
-  const gameover = battleStatsA === 0 || battleStatsB === 0;
+  const gameover = battleStatsA === 0 || battleStatsB === 0
   const winnerA = battleStatsB === 0
   const winnerB = battleStatsA === 0
 
-  const [pokemonHpScoreA, setPokemonHpScoreA ] = useState(0)
-  const [pokemonHpScoreB, setPokemonHpScoreB ] = useState(0)
+  const [pokemonHpScoreA, setPokemonHpScoreA] = useState(0)
+  const [pokemonHpScoreB, setPokemonHpScoreB] = useState(0)
 
+  const totalProgressBars = 1
+  const progressStatusArray = [1, 2, 3, 4, 5, 6]
+  const progressStatusArrayLength = progressStatusArray.length
 
-  const totalProgressBars = 1;
-  const progressStatusArray = [1, 2, 3, 4, 5, 6];
-  const progressStatusArrayLength = progressStatusArray.length;
-
-  let numberA = battleStatsA;
-  let numberB = battleStatsB;
+  let numberA = battleStatsA
+  let numberB = battleStatsB
 
   function pokemonId(max) {
     return Math.floor(Math.random() * max)
   }
 
   function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-    }
-  let randomNum = generateRandomNumber(1, 2);
+    return Math.floor(Math.random() * (max - min + 1) + min)
+  }
+  let randomNum = generateRandomNumber(1, 2)
 
   function scoreCount() {
     if (battleStatsB === 0) {
@@ -157,7 +155,7 @@ function PokemonContextComponent({children}) {
     }
   }
 
-  function reset(){
+  function reset() {
     setStartButton(false)
     setBattleStatsA(6)
     setBattleStatsB(6)
@@ -167,34 +165,34 @@ function PokemonContextComponent({children}) {
     return (
       <div
         style={{
-          display: "flex",
+          display: 'flex',
           backgroundColor: backgroundColorStyle,
           flex: flexValue,
-          padding: "1.5%"
+          padding: '1.5%',
         }}
       />
-    );
-  };
-  
+    )
+  }
+
   const colors = {
-    fire:'#ffdec1',
-    grass:'#c8ffcc',
-    electric:'#fff5c4',
-    water:'#c5edff',
-    ground:'#f6dbc1',
-    rock:'#d3d3b3',
-    fairy:'#f8d3ff',
-    poison:'#7dd78f',
-    bug:'#fac984',
-    dragon:'#78a0e9',
-    psychic:'#e5e97b',
-    flying:'#e4c0c0',
-    fighting:'#d9c499',
-    normal:'#ecf0f1',
-    dark:'#7dd78f',
-    ghost:'#f6dbc1',
-    ice:'#d9c499',
-    steel:'#c8ffcc',
+    fire: '#ffdec1',
+    grass: '#c8ffcc',
+    electric: '#fff5c4',
+    water: '#c5edff',
+    ground: '#f6dbc1',
+    rock: '#d3d3b3',
+    fairy: '#f8d3ff',
+    poison: '#7dd78f',
+    bug: '#fac984',
+    dragon: '#78a0e9',
+    psychic: '#e5e97b',
+    flying: '#e4c0c0',
+    fighting: '#d9c499',
+    normal: '#bcc3c4',
+    dark: '#7dd78f',
+    ghost: '#f6dbc1',
+    ice: '#d9c499',
+    steel: '#c8ffcc',
   }
 
   const icons = {
@@ -216,62 +214,58 @@ function PokemonContextComponent({children}) {
     ghost: ghost,
     ice: ice,
     steel: steel,
-    }
-
-  const data = {
-    startButton:startButton,
-    setStartButton:setStartButton,
-    battleStatsA:battleStatsA,
-    setBattleStatsA:setBattleStatsA,
-    battleStatsB:battleStatsB,
-    setBattleStatsB:setBattleStatsB,
-    reset:reset,
-    gameover:gameover,
-    scoreCount:scoreCount,
-    winnerA:winnerA,
-    winnerB:winnerB,
-    numberA:numberA,
-    numberB:numberB,
-    pokemonId:pokemonId,
-    generateRandomNumber:generateRandomNumber,
-    randomNum:randomNum,
-    ProgressDivs:ProgressDivs,
-    colors:colors,
-    icons:icons,
-    totalProgressBars:totalProgressBars,
-    progressStatusArrayLength:progressStatusArrayLength,
-    progressStatusArray:progressStatusArray,
-    pokemonBattleId:pokemonBattleId,
-    setPokemonBattleId:setPokemonBattleId,
-    pokemonHpScoreA:pokemonHpScoreA,
-    setPokemonHpScoreA:setPokemonHpScoreA,
-    pokemonHpScoreB:pokemonHpScoreB,
-    setPokemonHpScoreB:setPokemonHpScoreB,
-    active:active,
-    setActive:setActive,
-    pokemon:pokemon,
-    setPokemon:setPokemon,
-    results:results,
-    setResults:setResults,
-    pokeNames:pokeNames,
-    setPokeNames:setPokeNames,
-    firstPokemonResult:firstPokemonResult,
-    setFirstPokemonResult:setFirstPokemonResult,
-    secondPokemonResult:secondPokemonResult,
-    setSecondPokemonResult:setSecondPokemonResult,
-    pokemonSpecies:pokemonSpecies,
-    setPokemonSpecies:setPokemonSpecies,
-    pokemonSpeciesName:pokemonSpeciesName,
-    setPokemonSpeciesName:setPokemonSpeciesName,
-    abilityDescription:abilityDescription,
-    setAbilityDescription:setAbilityDescription,
   }
 
-  return (
-    <PokemonContext.Provider value={data}>
-      {children}
-      </PokemonContext.Provider>
-  )
+  const data = {
+    startButton: startButton,
+    setStartButton: setStartButton,
+    battleStatsA: battleStatsA,
+    setBattleStatsA: setBattleStatsA,
+    battleStatsB: battleStatsB,
+    setBattleStatsB: setBattleStatsB,
+    reset: reset,
+    gameover: gameover,
+    scoreCount: scoreCount,
+    winnerA: winnerA,
+    winnerB: winnerB,
+    numberA: numberA,
+    numberB: numberB,
+    pokemonId: pokemonId,
+    generateRandomNumber: generateRandomNumber,
+    randomNum: randomNum,
+    ProgressDivs: ProgressDivs,
+    colors: colors,
+    icons: icons,
+    totalProgressBars: totalProgressBars,
+    progressStatusArrayLength: progressStatusArrayLength,
+    progressStatusArray: progressStatusArray,
+    pokemonBattleId: pokemonBattleId,
+    setPokemonBattleId: setPokemonBattleId,
+    pokemonHpScoreA: pokemonHpScoreA,
+    setPokemonHpScoreA: setPokemonHpScoreA,
+    pokemonHpScoreB: pokemonHpScoreB,
+    setPokemonHpScoreB: setPokemonHpScoreB,
+    active: active,
+    setActive: setActive,
+    pokemon: pokemon,
+    setPokemon: setPokemon,
+    results: results,
+    setResults: setResults,
+    pokeNames: pokeNames,
+    setPokeNames: setPokeNames,
+    firstPokemonResult: firstPokemonResult,
+    setFirstPokemonResult: setFirstPokemonResult,
+    secondPokemonResult: secondPokemonResult,
+    setSecondPokemonResult: setSecondPokemonResult,
+    pokemonSpecies: pokemonSpecies,
+    setPokemonSpecies: setPokemonSpecies,
+    pokemonSpeciesName: pokemonSpeciesName,
+    setPokemonSpeciesName: setPokemonSpeciesName,
+    abilityDescription: abilityDescription,
+    setAbilityDescription: setAbilityDescription,
+  }
+
+  return <PokemonContext.Provider value={data}>{children}</PokemonContext.Provider>
 }
 
 export default PokemonContextComponent
